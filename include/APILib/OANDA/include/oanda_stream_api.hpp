bcc4febb4fdd0543e6e636a51f53cae01b824013
CupOfTeaJay <tjgiven17@protonmail.com>
2023-08-20 10:59:44 -0400

    implemented OandaStreamApi

---
+++ include/APILib/OANDA/include/oanda_stream_api.hpp
@@ -0,0 +1,34 @@
+/**
+ * \file oanda_stream_api.hpp
+ * 
+ * TODO:
+ */
+
+#pragma once
+#include "../../include/api.hpp"
+
+namespace Wyeth
+{
+    class OandaStreamApi : public Api
+    {
+        public:
+
+        /**
+         * Constructor for the "OandaApi" class.
+         */
+        OandaStreamApi(const std::string& authToken,
+                       const std::string& uri) noexcept;
+
+        /**
+         * Destructor for the "OandaApi" class.
+         */
+        virtual ~OandaStreamApi() = default;
+
+        private:
+
+        /**
+         * Initializes the Oanda REST API's endpoints.
+         */
+        virtual const EndpointMap initializeEndpoints() noexcept override final;
+    };
+}
